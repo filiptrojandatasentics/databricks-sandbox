705d0a6820e56b74e219d619cd16b845609c232c
filip <f.trojan@centrum.cz>
2024-04-01 19:56:49 +0200

    save as test.telco_customer_churn.input_table

---
+++ src/read_telco_customer_churn.py
@@ -0,0 +1,17 @@
+import openml
+from databricks.connect import DatabricksSession
+
+
+spark = DatabricksSession.builder.profile("filip.trojan@datasentics.com-cluster").getOrCreate()
+# https://www.openml.org/search?type=data&status=active&id=45568
+dataset = openml.datasets.get_dataset(45568)
+df, _, _, _ = dataset.get_data(dataset_format="dataframe")
+df["tenure"] = df["tenure"].astype(int)
+df.info()
+result = spark.sql("create schema if not exists test.telco_customer_churn")
+print(f"create schema: {result}")
+spark_df = spark.createDataFrame(df)
+spark_df.write.mode("overwrite").saveAsTable("test.telco_customer_churn.input_table")
+x = df.drop(columns=["Churn"])
+y = df["Churn"]
+# print(y.value_counts())
